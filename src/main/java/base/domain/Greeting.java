1dcf610002768d171a7f11b13745e4c355ce5296
yep606 <ilya.rogov.vladislav@gmail.com>
2020-02-12 08:12:41 +0300

    Initial commit

---
+++ src/main/java/base/domain/Greeting.java
@@ -0,0 +1,18 @@
+package base.domain;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
