75767166584f402ec0c7b937569cb90f79541ef9
yep606 <ilya.rogov.vladislav@gmail.com>
2020-04-16 23:28:09 +0300

    Clear

--- src/main/java/base/repo/MessageRepo.java
+++
@@ -1,7 +0,0 @@
-package base.repo;
-
-import base.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepo extends JpaRepository<Message, Long> {
-}
--- src/main/java/base/repo/UserDetailsRepo.java
+++
@@ -1,7 +0,0 @@
-package base.repo;
-
-import base.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsRepo extends JpaRepository<User, String > {
-}
