3d1ad2279f3c9b9637fe860a089fdc2a722531e6
yep606 <ilya.rogov.vladislav@gmail.com>
2020-04-22 20:19:00 +0300

    app js refactor

---
+++ src/main/resources/static/js/components/messages/MessagesList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div style="position: relative; width: 300px">
+        <message-form :messages="messages" :messageAttr="message"/>
+        <message-row v-for="message in messages"
+                     :key="message.id"
+                     :message="message"
+                     :messages="messages"
+                     :editMessage="editMessage"
+                     :deleteMessage="deleteMessage"/>
+    </div>
+</template>
+
+<script>
+    import MessageRow from "./MessageRow.vue"
+    import MessageForm from "./MessageForm.vue";
+    export default {
+        props: ['messages'],
+        components: {
+            MessageRow,
+            MessageForm
+        },
+        data() {
+            return {
+                message: null,
+            }
+
+        },
+        methods: {
+            editMessage(message) {
+                this.message = message;
+            },
+            deleteMessage(message){
+                this.$resource('/message/{id}').remove({id: message.id}).then(result => {
+                    if (result.ok) {
+                        this.messages.splice(this.messages.indexOf(this.message), 1);
+                    }
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -4,17 +4,24 @@
             <a href="/login">Google</a>
         </div>
         <div v-else>
-            <div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>
-<!--            <messages-list :messages="messages"/>-->
+            <div>{{profile.name}}&nbsp;<a href="/logout">Log out</a></div>
+            <messages-list :messages="messages"/>
         </div>
     </div>
 </template>
 
 <script>
+    import MessagesList from "../components/messages/MessagesList.vue";
     export default {
-        data: {
-            messages: frontendData.messages,
-            profile: frontendData.profile
+        components: {
+            MessagesList
+        },
+
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile,
+            }
         }
     }
 </script>
