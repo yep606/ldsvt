ee8e8b89499b4ed5b86353a1066af4021d8720db
yep606 <ilya.rogov.vladislav@gmail.com>
2020-04-23 14:41:11 +0300

    websocket support

--- src/main/java/base/controller/MessageController.java
+++ src/main/java/base/controller/MessageController.java
@@ -6,6 +6,8 @@ import base.repo.MessageRepo;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -25,40 +27,37 @@ public class MessageController {
     @GetMapping
     @JsonView(Views.IdName.class)
     public List<Message> list(){
-
         return repo.findAll();
-
     }
 
     @GetMapping("{id}")
     @JsonView(Views.FullMessage.class)
     public Message getOneMessage(@PathVariable("id") Message message){
-
         return message;
-
     }
 
     @PostMapping
     public Message createNew(@RequestBody Message message){
-
         message.setCreationData(LocalDateTime.now());
         return repo.save(message);
-
     }
 
     @PutMapping("{id}")
     public Message update(@PathVariable("id") Message messageFromDB, @RequestBody Message message){
-
         BeanUtils.copyProperties(message, messageFromDB, "id");
-
         return repo.save(messageFromDB);
-
     }
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable("id") Message message){
-
         repo.delete(message);
+    }
 
+
+    @MessageMapping("/changeMessage")
+    @SendTo("/topic/activity")
+    public Message change(Message message)
+    {
+        return repo.save(message);
     }
 }
